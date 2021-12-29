725ed935bd3df0766109b9b034ee7a54ed623fe8
cleber <cleber@cleber-G7>
2021-12-28 23:50:58 -0300

    init

---
+++ src/main/java/br/com/cleberbarros/cbteste/CbTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleberbarros.cbteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cleberbarros/cbteste/CbTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleberbarros.cbteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
